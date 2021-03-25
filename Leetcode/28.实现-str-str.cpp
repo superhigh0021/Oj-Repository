429e282b3fe2814f919f5e6ca5b89f3bb8842036
superhigh0021 <975440488@qq.com>
2021-03-26 00:02:14 +0800

    1

--- Leetcode/28.实现-str-str.cpp
+++ Leetcode/28.实现-str-str.cpp
@@ -7,36 +7,17 @@
 // @lc code=start
 class Solution {
 public:
-    int strStr(string haystack, string needle)
-    {
-        if (needle.size() == 0)
-            return 0;
-        if (needle.size() > haystack.size())
-            return -1;
-        int flag;
-        bool flag2 = false;
-        for (int i = 0; i < haystack.size(); ++i) {
-            if (haystack[i] == needle[0]) {
-                flag = i;
-                if (needle.size() > 1)
-                    for (int j = 1; j < needle.size();) {
-                        if (i + j < haystack.size()) {
-                            if (haystack[i + j] == needle[j]) {
-                                ++j;
-                                flag2 = true;
-                            } else {
-                                flag2 = false;
-                                break;
-                            }
-                        } else {
-                            flag2 = false;
-                            break;
-                        }
-                    }
-                else
-                    return flag;
-                if (flag2 == true)
-                    return flag;
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i <= m - n; i++) {
+            int j = 0;
+            for (; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
             }
         }
         return -1;
